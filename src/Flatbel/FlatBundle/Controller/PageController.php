c1ce3afe72872fccea62f65ec5ecd31c6a97a3e3
XMblr <mkirill97@gmail.com>
2017-02-25 20:44:58 +0300

    Second commit. Register done at 50%

--- src/Flatbel/FlatBundle/Controller/PageController.php
+++ src/Flatbel/FlatBundle/Controller/PageController.php
@@ -3,13 +3,39 @@
 
 namespace Flatbel\FlatBundle\Controller;
 
+use Flatbel\FlatBundle\Entity\Register;
+use Flatbel\FlatBundle\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FlatbelFlatBundle:Page:index.html.twig');
+    }
+
+    public function registerAction(Request $request)
+    {
+        $enquiry = new Register();
+
+        $form = $this->createForm(RegisterType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('FlatbelFlatBundle_register'));
+            }
+        }
+
+        return $this->render('FlatbelFlatBundle:Page:register.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 }
---
+++ src/Flatbel/FlatBundle/Form/RegisterType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Flatbel\FlatBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', EmailType::class);
+        $builder->add('password', PasswordType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'register';
+    }
+}
